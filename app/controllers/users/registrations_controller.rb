4acae9297d09ca9c39f8b8097d7786d535481de4
sakki1226 <142205272+sakki1226@users.noreply.github.com>
2023-11-11 17:32:15 +0900

    ログイン機能の修正９

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -41,6 +41,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       unless @user1.save && @user2.save
         render :new_user, status: :unprocessable_entity and return
       end
+      binding.pry
 
       @family_user.save
       session["family.regist_data"]["family"].clear
